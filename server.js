223b976e66d708efb3f048490e0b8c930bfea204
soundreaper <soundreaper@gmail.com>
2020-07-13 23:29:42 -0700

    hello world project init with navbar

---
+++ server.js
@@ -0,0 +1,22 @@
+// Initialize express
+const express = require('express')
+const exphbs = require('express-handlebars');
+const app = express()
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+// Tell our app to send the "hello world" message to our home page
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
