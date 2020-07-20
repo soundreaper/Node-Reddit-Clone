13da5fa28ee134a5c2168521400358c011310832
soundreaper <soundreaper@gmail.com>
2020-07-19 20:13:34 -0700

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 // Initialize express
 const express = require('express')
-const exphbs = require('express-handlebars');
 const app = express()
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+const exphbs = require('express-handlebars');
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+// Set db
+require('./data/reddit-db');
+
 // Tell our app to send the "hello world" message to our home page
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 })
+
+app.get('/posts/new', (req,res) =>{
+  res.render('posts-new')
+})
+
+require('./controllers/posts.js')(app);
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
