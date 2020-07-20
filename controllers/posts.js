ae289857ef1039acb305d13b8f62089b04f555be
soundreaper <soundreaper@gmail.com>
2020-07-19 23:05:13 -0700

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,50 +1,72 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports = (app) => {
+module.exports = app => {
+  // CREATE
+  app.post("/posts/new", (req, res) => {
+    if (req.user) {
+    
+      var post = new Post(req.body);
+      post.author = req.user._id;
 
-    // CREATE
-    app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
-
-        // SAVE INSTANCE OF POST MODEL TO DB
-        post.save((err, post) => {
-        // REDIRECT TO THE ROOT
-        return res.redirect(`/`);
-        })
-    });
-
-    // INDEX
-    app.get('/', (req, res) => {
-        Post.find({}).lean()
-        .then(posts => {
-            res.render("posts-index", { posts });
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-    });
-
-    // SHOW
-    app.get("/posts/:id", function(req, res) {
-        // LOOK UP THE POST
-        Post.findById(req.params.id).populate('comments')
+      post
+        .save()
         .then(post => {
-            res.render("posts-show", { post });
+            return User.findById(req.user._id);
+        })
+        .then(user => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          res.redirect(`/posts/${post._id}`);
         })
         .catch(err => {
             console.log(err.message);
         });
-    });
+    } else {
+      return res.status(401); // UNAUTHORIZED
+    }
+  });
+  
+  // INDEX
+  app.get('/', (req, res) => {
+    var currentUser = req.user;
+    console.log(req.cookies);
+    
+    Post.find({})
+    .populate('author')
+      .then(posts => {
+        res.render("posts-index", { posts, currentUser });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }); 
+  
+  //SHOW
+  app.get("/posts/:id", function (req, res) {
+    var currentUser = req.user;
+    // LOOK UP THE POST
 
-    // SUBREDDIT
-    app.get("/n/:subreddit", function(req, res) {
-        Post.find({ subreddit: req.params.subreddit })
+    Post.findById(req.params.id).populate('comments').populate('author')
+        .then(post => {
+            res.render("posts-show", { post, currentUser });  
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+  });
+  
+  // SUBREDDIT
+  app.get("/n/:subreddit", function (req, res) {
+    var currentUser = req.user;
+    Post.find({ subreddit: req.params.subreddit }).populate('author')
         .then(posts => {
-            res.render("posts-index", { posts });
+            res.render("posts-index", { posts, currentUser });
         })
         .catch(err => {
             console.log(err);
         });
-    });
+  });
+  
 };
