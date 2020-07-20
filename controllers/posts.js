8422d9162d9728a3ef42adbb78845467caf2ca7e
soundreaper <soundreaper@gmail.com>
2020-07-19 21:36:35 -0700

    Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,39 +2,49 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
-    })
-  });
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+        // REDIRECT TO THE ROOT
+        return res.redirect(`/`);
+        })
+    });
 
-  // INDEX
-  app.get('/', (req, res) => {
-    Post.find({}).lean()
-      .then(posts => {
-        res.render("posts-index", { posts });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  });
+    // INDEX
+    app.get('/', (req, res) => {
+        Post.find({}).lean()
+        .then(posts => {
+            res.render("posts-index", { posts });
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+    });
 
-  // SHOW
-  app.get("/posts/:id", function(req, res) {
-    // LOOK UP THE POST
-    Post.findById(req.params.id)
-      .then(post => {
-        res.render("posts-show", { post });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  });
+    // SHOW
+    app.get("/posts/:id", function(req, res) {
+        // LOOK UP THE POST
+        Post.findById(req.params.id)
+        .then(post => {
+            res.render("posts-show", { post });
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+    });
 
+    // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+        Post.find({ subreddit: req.params.subreddit })
+        .then(posts => {
+            res.render("posts-index", { posts });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    });
 };
